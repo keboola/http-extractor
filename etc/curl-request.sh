6d178ec2199cbe45bafbaa809adff08fdb219203
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-01-31 11:08:24 +0100

    Remove etc

--- etc/curl-request.sh
+++
@@ -1,1 +0,0 @@
-curl --request POST   --url https://syrup.keboola.com/docker/sandbox   --header 'Content-Type: application/json'   --header 'X-StorageApi-Token:4236-113722-72211aeaebd8c781aa52d4f9dbd1c394b15f5b6e'   --data '{	"configData": {  "parameters": {"downloadUrl": null}}}'
