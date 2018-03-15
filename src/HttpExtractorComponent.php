0f27e952959d2e3bafe8cddab4d0fe1187e2b317
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-03-15 14:56:09 +0100

    Test and fix slashes behavior
    Fixes #9

--- src/HttpExtractorComponent.php
+++ src/HttpExtractorComponent.php
@@ -12,6 +12,16 @@ use Psr\Http\Message\UriInterface;
 
 class HttpExtractorComponent extends BaseComponent
 {
+    public static function joinPathSegments(string $firstPart, string $secondPart): string
+    {
+        $separator = '/';
+
+        $baseWithoutTrailing = rtrim($firstPart, $separator);
+        $pathWithoutLeading = ltrim($secondPart, $separator);
+
+        return $baseWithoutTrailing . $separator . $pathWithoutLeading;
+    }
+
     protected function getConfigDefinitionClass(): string
     {
         return ConfigDefinition::class;
@@ -27,14 +37,20 @@ class HttpExtractorComponent extends BaseComponent
         $client = new Client();
         $httpExtractor = new HttpExtractor($client);
 
-        /** @var Config $config */
-        $config = $this->getConfig();
-        $uri = new Uri($config->getBaseUrl() . $config->getPath());
+        $uri = new Uri($this->getDownloadUrl());
         $httpExtractor->extract($uri, $this->getDestination($uri));
     }
 
     private function getDestination(UriInterface $uri): string
     {
         return $this->getDataDir() . '/out/files/' . basename($uri->getPath());
+    }
+
+    private function getDownloadUrl(): string
+    {
+        /** @var Config $config */
+        $config = $this->getConfig();
+
+        return self::joinPathSegments($config->getBaseUrl(), $config->getPath());
     }
 }
