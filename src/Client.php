7a51e6fa3a41aeb6774edef3a707f13ba3f90723
Michal Jurečko <michal.jurecko@gmail.com>
2021-07-21 12:17:30 +0200

    Add loger and set timeouts

--- src/Client.php
+++ src/Client.php
@@ -5,8 +5,13 @@ declare(strict_types=1);
 namespace Keboola\HttpExtractor;
 
 use GuzzleHttp\HandlerStack;
+use GuzzleHttp\MessageFormatter;
+use GuzzleHttp\Middleware;
 use Keboola\HttpExtractor\Client\ExponentialDelay;
 use Keboola\HttpExtractor\Client\RetryDecider;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Handler\StreamHandler;
+use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 
 class Client extends \GuzzleHttp\Client
@@ -19,7 +24,23 @@ class Client extends \GuzzleHttp\Client
             $config['handler'] = HandlerStack::create();
         }
         $stack = $config['handler'];
-        $stack->push(\GuzzleHttp\Middleware::retry(new RetryDecider($logger), new ExponentialDelay()));
+
+        // Timeouts
+        $config['connect_timeout'] = 60; // 60 seconds
+        $config['timeout'] = 15 * 60 * 60; // 15 minutes
+
+        // Retry
+        $stack->push(Middleware::retry(new RetryDecider($logger), new ExponentialDelay()));
+
+        // Logger
+        $logger = new Logger('logger');
+        $handler = new StreamHandler('php://stdout', Logger::INFO);
+        $handler->setFormatter(new LineFormatter("%message%\n"));
+        $logger->pushHandler($handler);
+        $messageFormatter = new MessageFormatter(
+            '{code} {phrase}: {method} "{target}"'
+        );
+        $stack->push(Middleware::log($logger, $messageFormatter));
 
         parent::__construct($config);
     }
